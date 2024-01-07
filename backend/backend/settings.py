0fa85bc016ac6bc59f3e11db94feb7e4b16a5f76
Александр Хегде <turnpace1000@gmail.com>
2024-01-07 22:16:01 +0300

    46

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = True
+DEBUG = (os.getenv('DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.17.244', 'iceadmin.ru']
 
