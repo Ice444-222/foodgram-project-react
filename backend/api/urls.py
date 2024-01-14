847f40be6a397cf6e54dd2dd5543d6537f0ad0ab
Александр Хегде <turnpace1000@gmail.com>
2024-01-14 19:02:33 +0300

    119

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from users.views import CustomTokenObtainPairView
 
 from .views import (IngredientViewSet, RecipeViewSet, TagViewSet,
-                    TokenLogoutView, UserViewSet, SubscribeUserAPIView)
+                    TokenLogoutView, UserViewSet)
 
 router = DefaultRouter()
 router.register("users", UserViewSet, basename="users")
